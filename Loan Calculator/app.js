fa254e511cdaf9f1d2daac46cba25bb836120bfd
Adela Ciucu <140815603+Adela-Ciucu@users.noreply.github.com>
2024-08-30 21:43:51 +0300

    Add files via upload
    Adding code in JavaScript  for the way of showing or hiding the results and loader.

--- Loan Calculator/app.js
+++ Loan Calculator/app.js
@@ -1,10 +1,15 @@
-//Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+ document.getElementById('results').style.display = 'none';
+ document.getElementById('loading').style.display = 'block';
 
-//Calculate Results
+ setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
 function calculateResults(e){
   console.log('Calculating...')
-  //UI Variables
+  
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
@@ -16,7 +21,6 @@ function calculateResults(e){
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
-  //Compute montly payments
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal * x * calculatedInterest) / (x-1);
 
@@ -24,35 +28,32 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)- principal).toFixed(2);
+
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
-//Show Error
+
 function showError(error){
-  //Create a div
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
+
   const errorDiv = document.createElement('div');
 
-//Get element
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
 
-  //Add class
   errorDiv.className = 'alert alert-danger';
 
-  //Create text node and append to div
   errorDiv.appendChild(document.createTextNode(error));
 
-  //Insert error above heading
   card.insertBefore(errorDiv, heading);
 
-  //Clear error after 3 section
   setTimeout(clearError, 3000);
 }
 
-//Clear error
 function clearError(){
   document.querySelector('.alert').remove();
 }
